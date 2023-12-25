962f67a4f30e6d8ec0d847811bbf0226f148d601
Yahor Makedon <emakedonw@gmail.com>
2023-12-25 21:47:50 +0100

    reduce delay and remove printing listen notes for run command

--- java/src/main/java/com/ym/gameoflife/command/impl/RunCommand.java
+++ java/src/main/java/com/ym/gameoflife/command/impl/RunCommand.java
@@ -3,7 +3,6 @@ package com.ym.gameoflife.command.impl;
 import com.ym.gameoflife.command.Command;
 import com.ym.gameoflife.controller.application.GameManager;
 import com.ym.gameoflife.input.ConsoleReader;
-import com.ym.gameoflife.output.ConsolePrinter;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,12 +10,11 @@ import java.util.concurrent.TimeUnit;
  * @author Yahor Makedon
  */
 public class RunCommand implements Command {
-  private static final int DELAY_BEFORE_RUN_MS = 3000;
+  private static final int DELAY_BEFORE_RUN_MS = 2000;
   private final GameManager gameManager = GameManager.getInstance();
 
   @Override
   public void execute(ConsoleReader consoleReader) {
-    ConsolePrinter.printListenNote();
     delayBeforeRun();
     gameManager.run();
   }

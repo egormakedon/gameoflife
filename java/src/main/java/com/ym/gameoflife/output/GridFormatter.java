f48a64805cc0bdb48ca963f15e54e7f71ca79eb5
Yahor Makedon <emakedonw@gmail.com>
2023-12-26 20:31:38 +0100

    refactor grid formatter

--- java/src/main/java/com/ym/gameoflife/output/GridFormatter.java
+++ java/src/main/java/com/ym/gameoflife/output/GridFormatter.java
@@ -1,5 +1,6 @@
 package com.ym.gameoflife.output;
 
+import com.ym.gameoflife.entity.CellState;
 import com.ym.gameoflife.entity.Grid;
 import com.ym.gameoflife.util.Printable;
 
@@ -10,15 +11,16 @@ import java.util.stream.IntStream;
  * @author Yahor Makedon
  */
 public class GridFormatter {
+  private static final String LIVE_SYMBOL = "■";
   private static final String SYMBOL_DELIMITER = "";
   private static final String VERTICAL_DASH = "|";
   private static final String DASH = "-";
   private static final String SPACE = " ";
+  private boolean griding = true;
 
   public String format(Grid grid) {
     return formatGrid(grid);
   }
-
   private String formatGrid(Printable printable) {
     String notFormattedString = printable.toPrintString();
     String[] rows = notFormattedString.split(Grid.DELIMITER);
@@ -27,13 +29,18 @@ public class GridFormatter {
     final int capacity = notFormattedString.length() * 8;
     StringBuilder sb = new StringBuilder(capacity);
 
-    Arrays.stream(rows).forEach(
-      row -> sb.append(rowSeparator).append("\n").append(formattedRow(row)));
-    sb.append(rowSeparator).append("\n");
+    Arrays.stream(rows).forEach(row -> {
+      appendRowSeparator(sb, rowSeparator);
+      sb.append(formattedRow(row));
+    });
+    appendRowSeparator(sb, rowSeparator);
     return sb.toString();
   }
-
   private CharSequence rowSeparator(int rowLength) {
+    if (gridingOff()) {
+      return "";
+    }
+
     final int capacity = rowLength * 4;
     StringBuilder sb = new StringBuilder(capacity);
 
@@ -41,14 +48,57 @@ public class GridFormatter {
     sb.append(DASH);
     return sb;
   }
-
+  private void appendRowSeparator(StringBuilder sb, CharSequence rowSeparator) {
+    if (gridingOn()) {
+      sb.append(rowSeparator).append("\n");
+    }
+  }
   private CharSequence formattedRow(String row) {
     final int capacity = row.length() * 4;
     StringBuilder sb = new StringBuilder(capacity);
 
-    Arrays.stream(row.split(SYMBOL_DELIMITER)).forEach(
-      symbol -> sb.append(VERTICAL_DASH).append(SPACE).append(symbol).append(SPACE));
-    sb.append(VERTICAL_DASH).append("\n");
+    Arrays.stream(row.split(SYMBOL_DELIMITER))
+      .map(symbol -> symbol.equals(CellState.LIVE.toPrintString()) ? LIVE_SYMBOL : SPACE)
+      .forEach(symbol -> appendCell(sb, symbol));
+    appendEndingToFormattedRow(sb);
     return sb;
+  }
+  private void appendCell(StringBuilder sb, String symbol) {
+    if (gridingOn()) {
+      appendCellWithGriding(sb, symbol);
+    } else {
+      appendCellWithoutGriding(sb, symbol);
+    }
+  }
+  private void appendCellWithGriding(StringBuilder sb, String symbol) {
+    sb
+      .append(VERTICAL_DASH)
+      .append(SPACE)
+      .append(symbol)
+      .append(SPACE);
+  }
+  private void appendCellWithoutGriding(StringBuilder sb, String symbol) {
+    sb
+      .append(SPACE)
+      .append(symbol);
+  }
+  private void appendEndingToFormattedRow(StringBuilder sb) {
+    if (gridingOn()) {
+      sb.append(VERTICAL_DASH);
+    }
+    sb.append("\n");
+  }
+
+  public void gridOn() {
+    griding = true;
+  }
+  public void gridOff() {
+    griding = false;
+  }
+  private boolean gridingOn() {
+    return griding;
+  }
+  private boolean gridingOff() {
+    return !gridingOn();
   }
 }
